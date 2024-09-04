651d6736587a1ea7036d2e403007dbdbc61b286a
Kauã Lopes <lopeskazin@gmail.com>
2024-09-04 15:52:00 -0300

    04/09
    terminando a pagina de menu

--- quickfood/src/screens/Home.jsx
+++ quickfood/src/screens/Home.jsx
@@ -5,6 +5,22 @@ import caminhaoimg from '../assets_imgs/how-to-work1.png'
 import lojaimg from '../assets_imgs/how-to-work2.png'
 import sinoimg from '../assets_imgs/how-to-work3.png'
 import imgpicanha_tophowitworks from '../assets_imgs/call-to-action-paralax.jpg'
+import imgdestaqnatural from '../assets_imgs/listing-logo03.png'
+import { CiStar } from "react-icons/ci";
+import iconestrela from '../assets_imgs/star.png'
+import imgdestaqmenu from '../assets_imgs/listing-logo09.png'
+import imgdestaqchefs from '../assets_imgs/listing-logo12.png'
+import imgdestaqrestaurant from '../assets_imgs/listing-logo13.png'
+import imgdestaqrestaurant2 from '../assets_imgs/listing-logo15.png'
+import imgdestaqifood from '../assets_imgs/listing-logo04.png'
+import imgfavorithearth from '../assets_imgs/hearth.png'
+import imgmenuinstagram from '../assets_imgs/instagram.png'
+import imgmenufacebook from '../assets_imgs/facebook.png'
+
+
+
+
+
 
 export default function Home() {
 
@@ -71,10 +87,10 @@ export default function Home() {
                         <div className='container_tittleimgs'>
                             <h3>Escolha um prato saboroso</h3>
                         </div>
-                        <div style={{width: '250px'}}>
+                        <div style={{ width: '250px' }}>
                             <p>Purus enim. Cras massa massa, maximus sit amet finibus quis, pharetra eu erat.</p>
                         </div>
-                        
+
                     </div>
                     <div className='tst'>
 
@@ -82,45 +98,226 @@ export default function Home() {
                         <div className='container_tittleimgs'>
                             <h3>Escolha um restaurante</h3>
                         </div>
-                        <div style={{width: '250px'}}>
+                        <div style={{ width: '250px' }}>
                             <p>Purus enim. Cras massa massa, maximus sit amet finibus quis, pharetra eu erat.</p>
                         </div>
-                       
+
                     </div>
                     <div className='tst'>
 
                         <div className='span_circlue'><img src={sinoimg} alt="" /></div>
                         <div className='container_tittleimgs'>
-                            <h3 className= 'style_writehowdo'>Entrega ou Delivery</h3>
+                            <h3 className='style_writehowdo'>Entrega ou Delivery</h3>
                         </div>
-                        <div style={{width: '250px'}}>
+                        <div style={{ width: '250px' }}>
                             <p>Purus enim. Cras massa massa, maximus sit amet finibus quis, pharetra eu erat.</p>
                         </div>
-        
+
                     </div>
                 </ul>
-                <div className= 'container_imgpicanha' style={{backgroundImage: `url(${imgpicanha_tophowitworks})` }}>
+                <div className='container_imgpicanha' style={{ backgroundImage: `url(${imgpicanha_tophowitworks})` }}>
                     <div className='container_gridpicanha'>
-                        <h2 style={{color: 'white', fontSize:'40px'}}>BASTA ENCOMENDAR E NÓS ENTREGAREMOS</h2>
-                            <p  className='teste2223' style={{color: 'white'}}>Pellentesque eget justo eget nibh luctus semper at ut tellus.</p>
+                        <h2 style={{ color: 'white', fontSize: '40px' }}>BASTA ENCOMENDAR E NÓS ENTREGAREMOS</h2>
+                        <p className='teste2223' style={{ color: 'white' }}>Pellentesque eget justo eget nibh luctus semper at ut tellus.</p>
                         <div className='teste2223'>
                             <button className='style_button-ordenaragora'>ORDENAR AGORA</button>
                         </div>
-                        
+
                     </div>
                 </div>
                 <div className='container_destaques'>
                     <div className='container_tittle-restaurantesemdestaque'>
-                        <h2 style={{fontSize: '30px'}}>RESTAURANTES EM DESTAQUE</h2>
+                        <h2 style={{ fontSize: '30px' }}>RESTAURANTES EM DESTAQUE</h2>
                         <p className='style-paragrafo-destaque'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                     </div>
+                    <div className='container_pai_restaurantedestaques'>
+                        <div className='container_restaurants-destaques'>
+                            <div className='container-in-destaque'>
+                                <div className='container-circle-destaqimg'>
+                                    <img className='style_imgs-destaques' src={imgdestaqrestaurant} alt="" />
 
-                    <div className='container_restaurants-destaques'>
-                        <div className='minicontainers-destaques' ></div>
+                                </div>
+                                <div className='container-text-about-restaurantdestaque'>
+                                    <h4 className='style-h4-destaq'>Restaurant</h4>
+                                    <small className='style_smalltext'>
+                                        Apple Juice, BB.Q
+                                        <br />
+                                        <br />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        (1) Review
+                                    </small>
+                                </div>
+                                <div className='container-favoritar-destaque'>
+                                    <img className='style-heartfavorit' src={imgfavorithearth} alt="" />
+                                </div>
+                            </div>
+                            <div className='container-in-destaque'>
+                                <div className='container-circle-destaqimg'>
+                                    <img className='style_imgs-destaques' src={imgdestaqnatural} alt="" />
+
+                                </div>
+                                <div className='container-text-about-restaurantdestaque'>
+                                    <h4 className='style-h4-destaq'>Natural Healthy Food</h4>
+                                    <small className='style_smalltext'>
+                                        Suco de maçã, carne assada, hambúrguer de queijo
+                                        <br />
+                                        <br />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        (1) Review
+                                    </small>
+                                </div>
+                                <div className='container-favoritar-destaque'>
+                                    <img className='style-heartfavorit' src={imgfavorithearth} alt="" />
+                                </div>
+                            </div>
+
+                            <div className='container-in-destaque'>
+                                <div className='container-circle-destaqimg'>
+                                    <img className='style_imgs-destaques' src={imgdestaqmenu} alt="" />
+
+                                </div>
+                                <div className='container-text-about-restaurantdestaque'>
+                                    <h4 className='style-h4-destaq'>Menu & Drinks</h4>
+                                    <small className='style_smalltext'>
+                                        Chicken Roast, Chines Soup, Cold Coffee
+                                        <br />
+                                        <br />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        (1) Review
+                                    </small>
+                                </div>
+                                <div className='container-favoritar-destaque'>
+                                    <img className='style-heartfavorit' src={imgfavorithearth} alt="" />
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className='container_restaurants-destaques2'>
+                            <div className='container-in-destaque'>
+                                <div className='container-circle-destaqimg'>
+                                    <img className='style_imgs-destaques' src={imgdestaqchefs} alt="" />
+
+                                </div>
+                                <div className='container-text-about-restaurantdestaque'>
+                                    <h4 className='style-h4-destaq'>Chefs</h4>
+                                    <small className='style_smalltext'>
+                                        Suco de maçã, carne assada, hambúrguer de queijo
+                                        <br />
+                                        <br />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        (1) Review
+                                    </small>
+                                </div>
+                                <div className='container-favoritar-destaque'>
+                                    <img className='style-heartfavorit' src={imgfavorithearth} alt="" />
+                                </div>
+                            </div>
+                            <div className='container-in-destaque'>
+                                <div className='container-circle-destaqimg'>
+                                    <img className='style_imgs-destaques' src={imgdestaqrestaurant2} alt="" />
+
+                                </div>
+                                <div className='container-text-about-restaurantdestaque'>
+                                    <h4 className='style-h4-destaq'>Menu's</h4>
+                                    <small className='style_smalltext'>
+                                        Fish Fry, Fresh Juice, Stakes
+                                        <br />
+                                        <br />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        () Review
+                                    </small>
+                                </div>
+                                <div className='container-favoritar-destaque'>
+                                    <img className='style-heartfavorit' src={imgfavorithearth} alt="" />
+                                </div>
+                            </div>
+                            <div className='container-in-destaque'>
+                                <div className='container-circle-destaqimg'>
+                                    <img className='style_imgs-destaques' src={imgdestaqifood} alt="" />
+
+                                </div>
+                                <div className='container-text-about-restaurantdestaque'>
+                                    <h4 className='style-h4-destaq'>Food N&H</h4>
+                                    <small className='style_smalltext'>
+                                        Beef Roast, Cheese Burger, Doughnut
+                                        <br />
+                                        <br />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        <img className='style-estrelaimg' src={iconestrela} alt="" />
+                                        (4) Review
+                                    </small>
+                                </div>
+                                <div className='container-favoritar-destaque'>
+                                    <img className='style-heartfavorit' src={imgfavorithearth} alt="" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
+
+                <div className='container_cabecalho-informacoes'>
+                    <div className='container_infor-text'>
+                        <div className='container-menus-tela'>
+                            <div className='tittle-menus'>
+                                <h3>Popular Cities</h3>
+                                {/* <p className='style-barra'></p> */}
+                            </div>
+                            <div></div>
+                        </div>
+
+                        <div className='container-menus-tela'>
+                            <div className='tittle-menus'>
+                                <h3>Popular Cuisnies</h3>
+                                {/* <p className='style-barra'></p> */}
+                            </div>
+                        </div>
+
+                        <div className='container-menus-tela'>
+                            <div className='tittle-menus'>
+                                <h3>Menus</h3>
+                                {/* <p className='style-barra'></p> */}
+                            </div>
+                        </div>
+
+                        <div className='container-menus-tela'>
+                            <div className='tittle-menus'>
+                                <h3>Contact</h3>
+                                {/* <p className='style-barra'></p> */}
+                            </div>
+                            <div className='container_icons'>
+                                <img src={imgmenufacebook} alt="" />
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
 
         </div>
     )
