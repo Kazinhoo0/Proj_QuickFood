5cdf13dbbe05a5dc1ac147326826cdec0a7a17b0
Kazinh_o <lopeskazin@gmail.com>
2024-09-30 21:39:41 -0300

    .
    .

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const secretkey = 'K2$%542!!'
 
 
 // Servir arquivos estáticos da pasta build
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 
 
